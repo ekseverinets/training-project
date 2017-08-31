e909486df9e58ba07edb02805fdcec6b0eee3d9f
Evgeniya Severinets <ekseverinets@gmail.com>
2017-08-31 18:07:47 +0300

    Created login form

--- server.js
+++ server.js
@@ -3,10 +3,61 @@ var app = express();
 var mongojs = require('mongojs');
 var db = mongojs('contactlist',['contactlist']);
 var bodyParser = require('body-parser');
+var sessions = require('express-session');
+
+var session;
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(sessions({
+	secret: '123456789',
+	resave: false,
+	saveUninitialized: true
+}))
+
+app.get('/login', function(req, res) {
+	if(session.uniqueID) {
+		res.redirect('/redirects');
+	}
+	res.sendFile('../public/templates/login.html', {root: __dirname});
+});
+
+app.post('/login', function(req, res) {
+	session = req.session;
+	if(session.uniqueID) {
+		res.redirect('/redirects');
+	}
+	session.uniqueID = req.body.username;
+	res.redirect('/redirects');
+});
+
+app.get('/logout', function(req, res){
+	req.session.destroy();
+	res.redirect('/#!/login');
+});
+
+app.get('/admin', function(req, res) {
+	session = req.session;
+	if(session.uniqueID != 'admin') {
+		res.send('Unauthorized access <a href="/logout">Try again</a>');
+	}
+});
+
+app.get('/redirects', function(req, res) {
+	session = req.session;
+	if(session.uniqueID == 'admin') {
+		console.log(session.uniqueID);
+		res.redirect('/#!/contactlist');
+	} else {
+		res.send(req.session.uniqueID + ' not found <a href="/logout">Try again</a>');
+	}
+});
+
+
+/* Contacts List */
 app.get('/contactlist', function (req, res) {
     console.log("I received a GET request");
 
